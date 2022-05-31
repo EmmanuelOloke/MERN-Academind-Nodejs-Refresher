d2ef85e60ff36ab5a456642d6b057179e9d2623c
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-05-31 01:50:14 +0100

    using express to properly parse data in the app with body-parser

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    let body = '';
-    req.on('end', () => {
-        const userName = body.split('=')[1];
-        if (userName) {
-            req.body = { name: userName };
-        }
-        next();
-    });
-    req.on('data', chunk => {
-        body += chunk;
-    });
+app.use(bodyParser.urlencoded({ extended: false })); //This will parse all incoming request and try to extract any data which is in the body if it's of type URLEncoded data, which will be the case for the submitted form. It will also automatically move on to the next middleware after it ie Call next()
+
+app.post('/user', (req, res, next) => {
+    res.send('<h1>User: ' + req.body.username + '</h1>');
 })
 
-app.use((req, res, next) => {
-    if (req.body) {
-        return res.send('<h1>' + req.body.name + '</h1>');
-    }
-    res.send('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
+app.get('/', (req, res, next) => {
+    res.send('<form action="/user" method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
 })
 
 app.listen(4000);
